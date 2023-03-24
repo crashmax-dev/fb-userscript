7f9ac379ec0d0fc8aa940467eedd6f6cb1e165b4
crashmax-dev <crashmax.dev@gmail.com>
2023-03-24 16:09:27 +0000

    Deploying to gh-pages from @ crashmax-dev/fb-userscript@71364c7221ad0a36dd933bb29bac42427ecd56b6 🚀

---
+++ fb-userscript.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        fb-userscript
+// @version     0.0.1
+// @license     MIT
+// @homepage    https://crashmax-dev.github.io/fb-userscript/
+// @match       http://localhost:3000/*
+// @match       https://review.intern.facebook.com/*
+// @grant       GM_addStyle
+// @updateURL   https://crashmax-dev.github.io/fb-userscript/fb-userscript.meta.js
+// @downloadURL https://crashmax-dev.github.io/fb-userscript/fb-userscript.user.js
+// ==/UserScript==
